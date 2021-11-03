8cb576cb72c2bb440c5614ce5fbc0c178859f49b
Aakena <srinivasaaken@gmail.com>
2021-11-03 16:19:41 +0530

    Add project files.

---
+++ Part1-2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace Part1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string to reverse:");
+            string userInput = Console.ReadLine();
+            StringBuilder output = new StringBuilder();
+            for(int i= userInput.Length - 1; i>=0;i--)
+            {
+                output.Append(userInput[i]);
+            }
+            Console.WriteLine(userInput + " reverse string is " + output.ToString());
+        }
+    }
+}
